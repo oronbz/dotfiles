cbaf24f19c2da42d09cb6a28782f35a581078b29
Oron Ben Zvi <oronbz@gmail.com>
2026-08-31 15:36:08 +0300

    nvim: remove xcodebuild integration and DAP

--- nvim/.config/LazyVim/lua/config/autocmds.lua
+++ nvim/.config/LazyVim/lua/config/autocmds.lua
@@ -6,27 +6,6 @@
 --
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
-vim.api.nvim_create_autocmd("User", {
-  pattern = { "XcodebuildBuildFinished", "XcodebuildTestsFinished" },
-  callback = function(event)
-    if event.data.cancelled then
-      return
-    end
-
-    if event.data.success then
-      require("trouble").close()
-    elseif not event.data.failedCount or event.data.failedCount > 0 then
-      if next(vim.fn.getqflist()) then
-        require("trouble").open("quickfix")
-      else
-        require("trouble").close()
-      end
-
-      require("trouble").refresh()
-    end
-  end,
-})
-
 vim.api.nvim_create_autocmd("BufReadPost", {
   group = vim.api.nvim_create_augroup("resolve_symlinks", { clear = true }),
   callback = function(args)
